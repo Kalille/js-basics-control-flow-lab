564b261f3e683650a518bbb025e9235bbdd2a5ed
kalille Overton <>
2021-04-08 20:09:39 -0400

    commit tree 1c7c1acf0f90e5e8210ac5384e4b8dfb8e8d94f6

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if (depth <= 400)
+  {return "This one is on me!"}
+  else if (depth <= 2499)
+  {return "I will gladly take your thirty bucks."}
+  else (depth >= 2500)
+  {return ("No can do.")}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(place){
   // Write your code here!
+ return ternaryCheckCity = place === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch (amount){
+    case "generous":
+      return "Thank you so much.";
+      break;
+      case "not as generous":
+        return "Thank you.";
+        break;
+        default:
+          return "Bye.";
+          break;
+  }
+
 }
